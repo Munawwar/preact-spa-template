daddadf74cf64563cea22cd18e9169c74a80e607
Munawwar <munawwarfiroz@hotmail.com>
2022-04-07 22:25:01 +0400

    generate sourcemap and bundle analysis file

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,20 @@ import preact from '@preact/preset-vite';
 import hash from 'string-hash';
 // eslint-disable-next-line import/no-unresolved
 import sassDts from 'vite-plugin-sass-dts';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact(), sassDts()],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      plugins:
+        process.env.NODE_ENV === 'production'
+          ? [visualizer({ filename: 'bundle-analysis.html' })]
+          : [],
+    },
+  },
   resolve: {
     alias: {
       '~': path.resolve(__dirname, '/src'),
